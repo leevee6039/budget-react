d4118d4d901bdf14ec960789504523e4a65ac2dc
leevee6039 <leevee6039@gmail.com>
2022-10-30 22:40:15 -0400

    used useState + mapping a array of objects with key + aded deleteEntry function + added newEntry function + toggle to identify the type of entry

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import './App.css';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
-import EntryLine from './components/EntryLine';
+import EntryLines from './components/EntryLines';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+
+  function deleteEntry(id) {
+    const result = entries.filter((entry) => entry.id !== id);
+    setEntries(result);
+  }
+
+  function addEntry(description, value, isExpense) {
+    const result = entries.concat({
+      id: entries.length + 1,
+      description,
+      value,
+      isExpense,
+    });
+    console.log('result', result);
+    console.log('entries', entries);
+    setEntries(result);
+  }
+
   return (
     <Container>
       <MainHeader title="Budget" />
@@ -16,13 +36,39 @@ function App() {
 
       <MainHeader title="History" type="h3" />
 
-      <EntryLine description="income" value="$10.00" />
-      <EntryLine description="expense" value="$10.00" isExpense />
+      <EntryLines entries={entries} deleteEntry={deleteEntry} />
 
       <MainHeader title="Add new transaction" type="h3" />
-      <NewEntryForm />
+      <NewEntryForm addEntry={addEntry} />
     </Container>
   );
 }
 
 export default App;
+
+var initialEntries = [
+  {
+    id: 1,
+    description: 'Work Income',
+    value: '$1000,00',
+    isExpense: false,
+  },
+  {
+    id: 2,
+    description: 'Water bill',
+    value: '$20,00',
+    isExpense: true,
+  },
+  {
+    id: 3,
+    description: 'Rent',
+    value: '$300,00',
+    isExpense: true,
+  },
+  {
+    id: 4,
+    description: 'Power bill',
+    value: '$50,00',
+    isExpense: true,
+  },
+];
